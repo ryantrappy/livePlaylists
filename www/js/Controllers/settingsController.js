6f4ad8e3d4535f8a7b20434203174bb555bdc70f
Ryan Trapp <ryantrapp@live.com>
2017-10-26 19:20:28 -0500

    now plays music from apple music (#1)

--- www/js/Controllers/settingsController.js
+++ www/js/Controllers/settingsController.js
@@ -15,6 +15,20 @@ angular.module('livePlaylists')
 
         $scope.appleMusicLogin = function(){
 
+            //After this move to someplace else
+            appleMusicPlugin.requestAuthorization(
+                function(isAuthorized){
+                    console.log("authorized");
+                    trackId = "https://itunes.apple.com/us/artist/jack-johnson/id909253?uo=4";
+                    appleMusicPlugin.playTrack(trackId,
+                        function(data){console.log("playing")},
+                        function(data){console.log(data)})
+
+                    //https://itunes.apple.com/us/artist/jack-johnson/id909253?uo=4
+                }, function(data){
+                    console.log(data);
+                    console.log("failed to authorize");
+                })
         };
 
         $scope.soundCloudLogin = function(){
